6182843eeb948a8f36fe6f752637ca5940ebb4f5
alexinav <66267480+alexinav@users.noreply.github.com>
2020-06-04 22:51:35 +0200

    Test error 404

--- Connection.cs
+++ Connection.cs
@@ -15,7 +15,7 @@ namespace FunctionSAS2020
         public CloudStorageAccount Create()
         {
 
-            var connectionString = "DefaultEndpointsProtocol=https;AccountName=alextorre;AccountKey=O8nwOWtm/XWyxDOeaD16J7QgFILBtdq7EOtft9a0mqVe+e6/xq5WUDT0KoTkYJwI8HIHimGng6PxE+KGb6+Bjw==;EndpointSuffix=core.windows.net";
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=alextorpe;AccountKey=O8nwOWtm/XWyxDOeaD16J7QgFILBtdq7EOtft9a0mqVe+e6/xq5WUDT0KoTkYJwI8HIHimGng6PxE+KGb6+Bjw==;EndpointSuffix=core.windows.net";
             var storageAccount = CloudStorageAccount.Parse(connectionString);
 
             return storageAccount;
@@ -30,7 +30,7 @@ namespace FunctionSAS2020
 //            var tokenCredential = new TokenCredential(accessToken);
 //            var storageCredentials = new TokenCredential(accessToken);
 //            //Define the blob to read
-//            var blob = new CloudBlockBlob(new Uri($"https://alextorre.blob.core.windows.net/containimg1/campus.jpg"), storageCredentials);
+//            var blob = new CloudBlockBlob(new Uri($"https://alextorpe.blob.core.windows.net/containimg1/campus.jpg"), storageCredentials);
 //            // Open a data stream to the blob
 //            return await blob.OpenReadAsync();
 //        }
