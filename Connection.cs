b0777147a6f8314a27c6884009dc88dfc95e269b
Alex Nazarenko <ais.jaz@hotmail.com>
2020-06-04 19:05:06 +0200

    Finished function Get Sas

--- Connection.cs
+++ Connection.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.WindowsAzure.Storage;
+﻿using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace FunctionSAS2020
 {
@@ -13,8 +18,21 @@ namespace FunctionSAS2020
             var connectionString = "DefaultEndpointsProtocol=https;AccountName=alextorre;AccountKey=O8nwOWtm/XWyxDOeaD16J7QgFILBtdq7EOtft9a0mqVe+e6/xq5WUDT0KoTkYJwI8HIHimGng6PxE+KGb6+Bjw==;EndpointSuffix=core.windows.net";
             var storageAccount = CloudStorageAccount.Parse(connectionString);
 
-            // var storageAccount
             return storageAccount;
         }
+//        private async Task<string> GetAccessTokenAsync()
+//        {
+//            var tokenProvider = new AzureServiceTokenProvider();
+//            return await tokenProvider.GetAccessTokenAsync("https://storage.azure.com/");
+//        }
+//        async Task<Stream> GetBlobWithSdk(string accessToken)
+//        {
+//            var tokenCredential = new TokenCredential(accessToken);
+//            var storageCredentials = new TokenCredential(accessToken);
+//            //Define the blob to read
+//            var blob = new CloudBlockBlob(new Uri($"https://alextorre.blob.core.windows.net/containimg1/campus.jpg"), storageCredentials);
+//            // Open a data stream to the blob
+//            return await blob.OpenReadAsync();
+//        }
     }
 }
--- FunctionSAS2020XUnitTest/getsas.cs
+++ FunctionSAS2020XUnitTest/getsas.cs
@@ -3,11 +3,21 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using FunctionSAS2020XUnitTest;
 using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace FunctionSAS2020
 {
     public class GetSasTest
     {
+        private readonly ILogger logger = TestFactory.CreateLogger();
+        [Fact]
+        public void GetSasDoNotEmpty()
+        {
+            var theresas = new GetSas().Do();
+
+            Assert.NotEmpty(theresas);
+        }
         [Fact]
         public void GetSasReturnInNotEmpty()
         {
@@ -31,7 +41,19 @@ namespace FunctionSAS2020
         {
             var sas = new GetSas().Do();
 
-            Assert.Contains("&sp=rl", sas);    //d
+            Assert.Contains("&sp=rl", sas);
+        }
+
+        [Fact]
+        public async void GetSasReturnASasWithPermitionOfRW()
+        {
+            var request = TestFactory.CreateHttpRequest("", "");
+            var response = await HelloWd.Run(request, logger);
+
+            var result = response as OkObjectResult;
+
+            Assert.Contains("?sv=", result.Value.ToString()); ;
+            Assert.Contains("&sp=rl", result.Value.ToString()); ;
         }
     }
 }
